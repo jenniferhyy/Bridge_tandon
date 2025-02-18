d484bf637e1d813e3789943e7db67ea681e9da1c
jenniferhyy <hoyuyanjennifer@gmail.com>
2025-02-18 14:42:12 +0800

    Update hw6_q3.cpp

--- hw6_q3.cpp
+++ hw6_q3.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 using namespace std; 
 
-int factor(int n);
 double eApprox(int n);
 
 int main(){
-  cout.precision(30);
-  for (int n = 1; n <= 15; n++) {
+    cout.precision(30);
+    for (int n = 1; n <= 15; n++) {
     cout<<"n = "<<n<<'\t'<<eApprox(n)<<endl;
-  }
-  return 0;
-}
-
-int factor(int n){
-  factor = 1;
-  for (int i = 1; i <= n; i++){
-    factor = factor * i;
-  }
-  return factor;
+    }
+    return 0;
 }
 
 double eApprox(int n){
   double e = 1.0;
+  double factor = 1.0;
   for (int i = 1; i <= n; i++){
-    e += 1.0/(factor(i));
+    factor = factor * i;
+    e += 1.0/factor;
   }
   return e;
 }
