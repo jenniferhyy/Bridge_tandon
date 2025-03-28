b37d054cf6c820af48aa9591591f0ef481fa03b9
jenniferhyy <hoyuyanjennifer@gmail.com>
2025-03-28 17:54:22 +0800

    Update sumofarray.cpp

--- sumofarray.cpp
+++ sumofarray.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int sumArrayR(int arr[], int arrsize){
+int sumArrayR1(int arr[], int arrsize){
   if (arrsize == 1){
     return arr[0];
   } else {
-    int temp = sumArrayR(arr, arrsize-1);
+    int temp = sumArrayR1(arr, arrsize-1);
     return temp + arr[arrsize-1];
+  }
+}
+
+int sumArrayR2(int arr[], int arrsize){
+  if (arrsize == 1){
+    return arr[0];
+  } else {
+   int temp = sumArrayR2(arr+1, arrsize-1);
+    return arr[0] + temp;
   }
 }
 
 int main(){
   int input[] = {10, 2, -6, 9, 11, 3};
   int size = 6;
-  cout << sumArrayR(input, size);
+  cout << sumArrayR1(input, size);
+  cout << sumArrayR2(input, size);
   return 0;
 }
